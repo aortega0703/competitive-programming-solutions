cad5c0f04453359a6b5f08eb5be445e665d3a618
Andrés Ortega <aortega0703@gmail.com>
2026-01-30 11:36:05 -0500

    Improve euler0719

--- project_euler/euler0719.py
+++ project_euler/euler0719.py
@@ -3,19 +3,14 @@ import math
 def count_digits(n):
     return int(math.log10(n)) + 1
 
-def split(square, square_digits, root, root_digits, terms):
-    # print(f"{square},  {terms}, {root}")
-    if square_digits <= root_digits and root == sum(terms) + square:
-        return [square] + terms
-    if square_digits < 2:
-        return None
-    for b_digits in range(1, root_digits + 1):
-        (a, b) = divmod(square, 10**b_digits)
-        a_digits = square_digits - b_digits
-        result = split(a, a_digits, root, root_digits, [b] + terms)
-        if result != None:
-            return result
-    return None
+def split(C, S, C_digits):
+    if C <= S:
+        return C == S
+    for b_digits in range(1, C_digits):
+        (a, b) = divmod(C, 10**b_digits)
+        if split(a, S - b, C_digits - b_digits):
+            return True
+    return False
 
 def main():
     max_digits = int(input())
@@ -27,20 +22,16 @@ def main():
     square_digits = count_digits(square)
     while square_digits <= max_digits:
         # (a + b) % 9 = 0
-        root_digits = (square_digits + 1) // 2
-        m = split(square, square_digits, root, root_digits, [])
-        if m != None:
-            print(f"{square} = {root}^2 = ({' + '.join(map(str,m))})^2")
+        if split(square, root, square_digits):
+            print(f"{square}", square_digits)
             ans += square
 
         # (a + b) % 9 = 1
         root += 1
         square = root * root
         square_digits = count_digits(square)
-        root_digits = (square_digits + 1) // 2
-        m = split(square, square_digits, root, root_digits, [])
-        if m != None:
-            print(f"{square} = {root}^2 = ({' + '.join(map(str,m))})^2")
+        if split(square, root, square_digits):
+            print(f"{square}", square_digits)
             ans += square
 
         # (a + b) % 9 = 0 again
